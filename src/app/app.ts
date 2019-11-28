28fc135b6e06a635b77384d6b6b82016de79d4f0
Chris Meyers <cm.02.93@gmail.com>
2019-11-27 19:02:18 -0700

    added types to all variables.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { Greeter } from './Greeter';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app: express.Application = express();
+const port: number = Number(process.env.PORT) || 3000;
 
-app.get('/', (req, res) => {
-  let name = req.query.name || 'world';
-  let greeter = new Greeter(name);
+app.get('/', (req: express.Request, res: express.Response) => {
+  let name: string = req.query.name || 'world';
+  let greeter: Greeter = new Greeter(name);
   res.json({'greeting': greeter.greet()});
 });
 
