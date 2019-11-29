1f0ed3d945dfb2850fe1057a9e1d8c327c58a8a3
Chris Meyers <cm.02.93@gmail.com>
2019-11-28 22:16:55 -0700

    simplified moduleNameMapper by using ts-jest helper.

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,10 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  // testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  // testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '@app(.*)$': '<rootDir>/app/$1'
-  }
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
